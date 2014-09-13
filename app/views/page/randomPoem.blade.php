9bf88db45307bf434051fe6234f8572c5b1d46aa
Tobias Føyn Føyen <tobias.foyen@gmail.com>
2014-09-13 12:56:16 +0200

    Finished styling of split button

--- app/views/page/randomPoem.blade.php
+++ app/views/page/randomPoem.blade.php
@@ -2,7 +2,10 @@
     <div class="columns small-12 small-centered medium-7 medium-centered large-5 large-centered">
         <div class="row">
             <div class="form-action columns small-6">
-                <a href="#" class="button split">Les et til <span class="split-button-icon" data-dropdown="drop"></span></a>
+                <div class="flex-button">
+                    <a href="#" class="button split">Les et til</a>
+                    <span class="split-button-icon" data-dropdown="drop"></span>
+                </div>
                 <ul id="drop" class="f-dropdown" data-dropdown-content>
                     <li><a href="#">Alle</a></li>
                     <li><a href="#">Nyeste</a></li>
