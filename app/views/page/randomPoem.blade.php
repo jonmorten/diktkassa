e7a8f73e10e6d01d8e4cb1106f92d566dc6b3ced
Jon-Morten Kristiansen <jon.morten.kristiansen@gmail.com>
2014-09-14 17:10:14 +0200

    Random mode button: Add mode icon

--- app/views/page/randomPoem.blade.php
+++ app/views/page/randomPoem.blade.php
@@ -2,17 +2,44 @@
     <div class="columns small-12 small-centered medium-7 medium-centered large-5 large-centered">
         <div class="row">
             <div class="form-action columns small-6">
-                <div class="flex-button">
+                <div class="split-button-wrapper">
                     <?php if ($mode === 'nyeste') { ?>
-                        <a class="button" href="{{ URL::route('randomPoem') }}/nyeste">Nyeste</a>
+                        <a class="button" href="{{ URL::route('randomPoem') }}/nyeste">Les et til</a>
                     <?php } else { ?>
                         <a class="button" href="{{ URL::route('randomPoem') }}">Les et til</a>
                     <?php } ?>
-                    <span class="split-button-icon" data-dropdown="drop"></span>
+                    <span
+                        class="split-button-icon"
+                        data-dropdown="random-poem-mode-selector"
+                    >
+                        <?php if ($mode === 'nyeste') { ?>
+                            <i class="fa fa-calendar"></i>
+                        <?php } else { ?>
+                            <i class="fa fa-bars"></i>
+                        <?php } ?>
+                    </span>
                 </div>
-                <ul id="drop" class="f-dropdown" data-dropdown-content>
-                    <li><a href="{{ URL::route('randomPoem') }}">Alle</a></li>
-                    <li><a href="{{ URL::route('randomPoem') }}/nyeste">Nyeste</a></li>
+                <ul
+                    id="random-poem-mode-selector"
+                    class="f-dropdown"
+                    data-dropdown-content
+                >
+                    <li>
+                        <a href="{{ URL::route('randomPoem') }}">
+                            <?php if ($mode === 'alle') { ?>
+                                <i class="fa fa-caret-right"></i>
+                            <?php } ?>
+                            Alle
+                        </a>
+                    </li>
+                    <li>
+                        <a href="{{ URL::route('randomPoem') }}/nyeste">
+                            <?php if ($mode === 'nyeste') { ?>
+                                <i class="fa fa-caret-right"></i>
+                            <?php } ?>
+                            Siste
+                        </a>
+                    </li>
                 </ul>
             </div>
             <p class="form-action columns small-6">
